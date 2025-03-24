983c8dd6a6006759ee2159131faac916564b9b3e
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-03-24 17:45:25 +0000

    Add thapi_stop() to both source and sink

--- btx_sink/callbacks.c
+++ btx_sink/callbacks.c
@@ -10,7 +10,16 @@ void btx_thapi_start_callbacks(
    printf("Received btx_thapi_start_callbacks message\n");
 }
 
-void btx_register_usr_callbacks(void *btx_handle) {
-  btx_register_callbacks_thapi_start(btx_handle, &btx_thapi_start_callbacks);
+void btx_thapi_stop_callbacks(
+    void *btx_handle,
+    void *usr_data,
+    const char* name,
+    int vpid, int vtid) {
+
+   printf("Received btx_thapi_stop_callbacks message\n");
 }
 
+void btx_register_usr_callbacks(void *btx_handle) {
+  btx_register_callbacks_thapi_start(btx_handle, &btx_thapi_start_callbacks);
+  btx_register_callbacks_thapi_stop(btx_handle, &btx_thapi_stop_callbacks);
+}
--- btx_source/callbacks.c
+++ btx_source/callbacks.c
@@ -7,7 +7,9 @@ void btx_init_proc(void *btx_handle, void *usr_data) {
 void btx_push_usr_messages(void *btx_handle, void *usr_data, btx_source_status_t *status) {
   int vpid = 1;
   int vtid= 1;
+  btx_push_message_thapi_stop(btx_handle, vpid, vtid);
   btx_push_message_thapi_start(btx_handle, vpid, vtid);
+  btx_push_message_thapi_stop(btx_handle, vpid, vtid);
   *status = BTX_SOURCE_END;
 }
 
@@ -15,4 +17,3 @@ void btx_register_usr_callbacks(void *btx_handle) {
   btx_register_callbacks_initialize_processing(btx_handle, btx_init_proc);
   btx_register_callbacks_push_usr_messages(btx_handle, &btx_push_usr_messages);
 }
-
