024e17a6fa0b649ca4a6b6578d6d289699c83109
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-03-31 21:21:26 +0000

    Fix a few issues in toggle.yaml

--- btx_sink/callbacks.c
+++ btx_sink/callbacks.c
@@ -1,25 +1,25 @@
 #include <metababel/metababel.h>
 #include <stdio.h>
 
-void btx_thapi_start_callbacks(
+void btx_start_callbacks(
     void *btx_handle,
     void *usr_data,
-    const char* name,
+    long int cpuid,
     int vpid, int vtid) {
 
-   printf("Received btx_thapi_start_callbacks message\n");
+   printf("Received btx_start_callbacks message\n");
 }
 
-void btx_thapi_stop_callbacks(
+void btx_stop_callbacks(
     void *btx_handle,
     void *usr_data,
-    const char* name,
+    long int cpuid,
     int vpid, int vtid) {
 
-   printf("Received btx_thapi_stop_callbacks message\n");
+   printf("Received btx_stop_callbacks message\n");
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
-  btx_register_callbacks_thapi_start(btx_handle, &btx_thapi_start_callbacks);
-  btx_register_callbacks_thapi_stop(btx_handle, &btx_thapi_stop_callbacks);
+  btx_register_callbacks_lttng_ust_toggle_start(btx_handle, &btx_start_callbacks);
+  btx_register_callbacks_lttng_ust_toggle_stop(btx_handle, &btx_stop_callbacks);
 }
